c0b7dce9b2ba8113e725322cb2e596c5bbbc2e72
Anirudh Ravishankar <onirudh3@gmail.com>
2024-01-14 16:03:20 +0100

    ughhh

--- sml_procedure_berry_cleaned.R
+++ sml_procedure_berry_cleaned.R
@@ -1,6 +1,15 @@
 
-##### Download the data
-matdata <- read.csv("filetoread", header = T, sep = ";", dec = ".")
+# Libraries and data ------------------------------------------------------
+
+library(MASS)
+library(data.table)
+library(nloptr)
+library(parallel)
+library(dplyr)
+options(digits = 5)
+
+# Data
+matdata <- read.csv("entry_data.csv", header = T, sep = ",", dec = ".")
 
 # Number of markets
 M = 1176
@@ -14,50 +23,34 @@ S = 1000
 # Number of cluster to make it in parallel
 Nclust = 40
 
-#### Libraries
-library(MASS)
-library(data.table)
-library(nloptr)
-library(parallel)
-library(dplyr)
-options(digits = 5)
-
-##### Simulation of error terms for SML N * M * S
+## Simulation of error terms for SML N * M * S
 set.seed(8769)
 uim = matrix(rnorm(N * M * S, mean = 0, sd = 1), N * M, S)
-u0m = matrix(rnorm(M * S, mean = 0, sd = 1), M, S) #### %*% matrix(rep(1, N), N, 1)
+u0m = matrix(rnorm(M * S, mean = 0, sd = 1), M, S) # %*% matrix(rep(1, N), N, 1)
 
 
-#### Explanatory variables
-#### Of course, you adapt your list here !!!!!!
-pop = sqrt(as.numeric(matdata$pop_Dep) * as.numeric(matdata$pop_Arr) / 1E6 / 1E6)
-City2 = (matdata$NbroutDep > 0) * (matdata$NbroutArr > 0)
-Nbroutes = (matdata$NbroutDep + matdata$NbroutArr) / 2
-Airportshare = (matdata$cityshareDep + matdata$cityshareArr) / 2
-dist = matdata$dist / 1000
+# Explanatory variables
+# Of course, you adapt your list here !!!!!
+pop = sqrt(as.numeric(matdata$popdep) * as.numeric(matdata$poparr) / 1E6 / 1E6)
+City2 = matdata$City2
+dist = matdata$direct_distance / 1000
 dist2 = dist ^ 2
-culture = ((matdata$cult_Dep + matdata$cult_Arr) > 1)
-sun = ((matdata$sun_Dep + matdata$sun_Arr) > 1)
-wealth = (matdata$gdppercap_Dep + matdata$gdppercap_Arr) / 2000
-Ncompet = matdata$N_2015
-LC = (matdata$Mainline_Low_Cost == "L") * 1
-Y = as.matrix(matdata$Pres)
+wealth = (matdata$gdppercapdep + matdata$gdppercaparr) / 2000
+Ncompet = matdata$nentrythreats
+Y = as.matrix(matdata$incumbent)
 X = as.matrix(rbind(rep(1, N * M), pop[1:(N * M)], wealth[1:(N * M)], dist[1:(N * M)],
-                    dist2[1:(N * M)], LC[1:(N * M)], sun[1:(N * M)], City2[1:(N * M)],
-                    Nbroutes[1:(N * M)]))
+                    dist2[1:(N * M)], City2[1:(N * M)]))
 matexpl = t(X[, 1:(N * M)])
 matN = Ncompet[seq(1, N * M, N)]
 
 mydata = data.frame(cbind(Y, matexpl))
-colnames(mydata) = c("Y", "K", "pop", "wealth", "dist", "dist2", "LC", "sun",
-                     "City2", "Nbroutes")
+colnames(mydata) = c("Y", "K", "pop", "wealth", "dist", "dist2", "City2")
 
-#### Nb var = col(matexpl) + 2 for the correlation of the term
+# Nb var = col(matexpl) + 2 for the correlation of the term
 nvar = ncol(matexpl) + 2
 
-
-#### Initial value
-myprobit <- glm(Y ~ pop + dist + dist2 + LC + sun + City2 + Nbroutes,
+# Initial value
+myprobit <- glm(Y ~ pop + dist + dist2 + City2,
                 family = binomial(link = "probit"),
                 data = mydata)
 
@@ -67,8 +60,7 @@ summary(myprobit)
 coefinit = c(coef(myprobit), 1, 0)
 
 
-
-#### Procedure to compute N at the equilibrium
+# Procedure to compute N at the equilibrium
 Calc_N <- function(s, matexpl, coef, uim, u0m){
   Calc_N_m <- function(m, s, matexpl, coef, uim, u0m){
 
@@ -86,14 +78,14 @@ Calc_N <- function(s, matexpl, coef, uim, u0m){
     nfirm = sum((sapply(seq(1, N, 1), above) - seq(1, N, 1)) > 0)
 
     return(c(1 * (nfirm == matN[m])))
-    }
+  }
 
   return(c(sapply(seq(1, M, 1), Calc_N_m, s = s, matexpl = matexpl, coef = coef,
                   uim = uim, u0m = u0m)))
-  }
+}
 
 
-#### Procedure to compute the log-likelihood
+# Procedure to compute the log-likelihood
 
 loglik <- function(theta){
 
